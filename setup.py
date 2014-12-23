17e7b6f0df94c31cebc6813549ffdad1a3b67372
MB <ubiqtus@gmail.com>
2014-12-23 16:13:51 -0500

    refactor and add sqlalchemy dependency

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='Phoenix Auto Sales Customer App',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='MB',
+      author_email='ubiqtus@gmail.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask>=0.7.2', 'MarkupSafe'],
+      install_requires=['Flask>=0.7.2', 'MarkupSafe', 'Flask-SQLAlchemy==0.16']
      )
